395a0ad2c14932e71e277d4d6b339cba2e9143e9
luisdanielmv <luisdanielmv@gmail.com>
2019-03-08 15:13:45 -0600

    late

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'createPost',
+    component: CreatePostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+import { UserService } from './services/user.service';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  // MatGridListModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import { RegisterComponent } from './register/register.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { ChipsInputComponent } from './shared/chips-input/chips-input.component';
+import { TagService } from './services/tag.service';
+import { PostService } from './services/post.service';
+import { PostCardComponent } from './shared/post-card/post-card.component';
+import { CommentComponent } from './shared/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostComponent,
+    ChipsInputComponent,
+    DashboardComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterComponent,
+    PostCardComponent,
+    CommentComponent,
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UserService, TagService, PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
